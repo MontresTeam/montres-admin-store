b9e7fc93a334e3e3b66a7b37f0fedfcbda9c4b87
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-10-15 06:53:52 -0500

    issue resolve

--- app/auth/create-password/page.jsx
+++ app/auth/create-password/page.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import Image from "next/image";
 import AuthImage from "@/public/assets/images/auth/forgot-pass-img.png";
 import ThemeLogo from "@/components/shared/theme-logo";
-import { StaticImg } from "@/types/static-image";
 import CreatePasswordComponent from "@/components/auth/create-password-component";
 
 
