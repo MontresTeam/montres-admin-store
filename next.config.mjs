3f74a94d576e0b091fcfc1241fee3936ac355bf1
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-11-08 03:29:01 -0500

    next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     images: {
     domains: ["technodsolutions.com","res.cloudinary.com","via.placeholder.com"], 
+    unoptimized: true, // ✅ placed correctly outside of domains array
   },
 };
 
